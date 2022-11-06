e3c7f3beaecc6f7e9f88ad01f3521b3c2381d6a9
Mimi <yupingxue@csus.edu>
2022-11-05 21:37:08 -0700

    Jira #64, 126. Mimi Darke. Format search results page. Authenticate account page, updated CSS.

---
+++ checkEmail.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Latest compiled JavaScript -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>California Mobility Center</title>
+</head>
+<body>
+ <section id = "title">
+        <nav class = "navbar navbar-expand-lg bg-Blue">
+            <a href="index.php" class = "navbar-brand"><img class="logo" src="image/CMC-logo-horizontal.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link text-white fs-4" href="./survey.html">Let's Start</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white fs-4 mx-3" href="./login.php">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </section>       
+	
+	<div class="d-flex justify-content-center my-5">
+        <div class="boxContent my-1">
+		      <p class="authenticate">Please check your email to authenticate your account. </p>
+			
+	  </div>
+    </div>
+</body>
+</html>
--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
     $host = "localhost";  
     $user = "root";  
     $password = '';  
-    $db_name = "testing";  
+    $db_name = "csc_190";  
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
