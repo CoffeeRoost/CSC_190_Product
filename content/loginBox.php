70393449f57e251bdde9905891a64655405fac5a
YOUR_USERNAME <YOUR_EMAIL_ADDRESS@EXAMPLE.COM>
2022-11-09 22:31:16 -0800

    - database vesion 2.2

--- content/loginBox.php
+++ content/loginBox.php
@@ -11,8 +11,8 @@
                 <label class="form-label" for="rememberMe">Remember me</label><br><br>
                 <button type="submit" class="btn btn-primary">Login</button><br>
                 <a class="text-decoration-none text-Blue" style="margin-left: 135px ;" href="./forgotPass.php">Forgot Password</a>
-                </form>  
+                </form>
                 <p class="text-center">Don't have an account?<span><a class="text-decoration-none text-Blue fs-4" href="survey.html"> Start Here </a></span></p>
             </div>
         </div>
-    </div>
+</div>
