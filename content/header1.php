edfe86eb67504742b41dd96bd24cbe52c778252c
student <student@localhost>
2022-11-12 16:20:50 -0800

    Gabby_C: changed reference in header1.php to connect to servey.php

--- content/header1.php
+++ content/header1.php
@@ -25,7 +25,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link text-white fs-4" href="./survey1.php">Let Start</a>
+                        <a class="nav-link text-white fs-4" href="./servey.php">Let Start</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-white fs-4 mx-3" href="./login.php">Login</a>
