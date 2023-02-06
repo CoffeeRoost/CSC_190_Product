7a4bf078b17ca15574b2bdc123ee68257e8b5d7d
Mimi <yupingxue@csus.edu>
2023-02-05 20:09:14 -0800

    Connect Login.php to backend authentication query
    Form components must use names and not ids to be visible in POST
    results

--- Login.php
+++ Login.php
@@ -1,4 +1,7 @@
 <?php
-    include_once('content/header2.php');
+	
+	include_once('content/header2.php');
     include_once('content/loginBox.php');
+	include_once('includes/login.inc.php');
+	
 ?>
--- content/loginBox.php
+++ content/loginBox.php
@@ -2,14 +2,14 @@
         <div class="boxContent my-1">
             <h1 class="text-center fw-bold my-5">Login</h1>
             <div class="fw-bold">
-                <form class="mx-5 mb-5" action="#">
+                <form class="mx-5 mb-5" action="#" method="post">
                 <label  class="form-label" for="email">Email</label><br>
-                <input class="form-control" type="email" id="email"><br>
+                <input class="form-control" type="email" name="email"><br>
                 <label class="form-label" for="password">Password</label><br>
-                <input class="form-control" type="password" id="password"><br>
+                <input class="form-control" type="password" name="password"><br>
                 <input type="checkbox" name="rememberMe" id="rememberMe">
                 <label class="form-label" for="rememberMe">Remember me</label><br><br>
-                <button type="submit" class="btn btn-primary">Login</button><br>
+                <button type="submit" class="btn btn-primary" name="login-submit">Login</button><br>
                 <a class="text-decoration-none text-Blue" style="margin-left: 135px ;" href="./forgotPass.php">Forgot Password</a>
                 </form>
                 <p class="text-center">Don't have an account?<span><a class="text-decoration-none text-Blue fs-4" href="./survey.php"> Start Here </a></span></p>
