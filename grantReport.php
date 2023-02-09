d516f2413411bb4f1806bb226779a0a11560912f
stevesyman <90881895+stevesyman@users.noreply.github.com>
2023-02-08 18:36:58 -0800

    CN-206 #time 4h 10m #comment main php to include grantReport from content #done

---
+++ grantReport.php
@@ -0,0 +1,10 @@
+<?php
+include_once('content/header4.php');
+?>
+
+<div class="d-flex">
+    <?php
+        include_once('content/sideBar1.php');
+        include_once('content/grantReport.php');
+    ?>
+</div>
