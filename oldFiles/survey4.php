7d838c7b66effe559dba010e70089f43bf7fd5a2
YOUR_USERNAME <79379392+ThuyDuong2@users.noreply.github.com>
2023-02-14 22:52:48 -0800

    #201 #215 #What this fix does -Added unit test: +#201: ContactTest.php +#215:TrainingTest.php -Removed oldFiles

--- oldFiles/survey4.php
+++
@@ -1,4 +0,0 @@
-<?php
-    include_once('content/header2.php');
-    include_once('content/surveyForm4.php');
-?>
--- oldFiles/survey5.php
+++
@@ -1,4 +0,0 @@
-<?php
-    include_once('content/header2.php');
-    include_once('content/surveyForm5.php');
-?>
