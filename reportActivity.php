13bfc0ff8029ce5a4c6c6e6d2a179da1e7b881bb
David Wen <davidwen@csus.edu>
2023-03-10 14:03:27 -0800

    CN-109 Connects TaskReport to Database

--- reportActivity.php
+++ reportActivity.php
@@ -5,6 +5,8 @@ include_once('content/header4.php');
 <div class="d-flex">
     <?php
         include_once('content/sideBar1.php');
-        include_once('content/reportActivity.php');
+        include_once('content/reportActivity.php');             //Frontend View
+        include_once('includes/dbh.inc.php');                   //Connect to database
+        include_once('includes/reportActivityToDatabase.php'); 
     ?>
 </div>
