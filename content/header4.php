bfdcdb319f00414df2661a368d4d4914ee8d76d9
Tinn Nguyen <tinnnguyen1deff5@gmail.com>
2023-03-09 23:27:25 -0800

    #253 fix connect and log out  session

--- content/header4.php
+++ content/header4.php
@@ -25,7 +25,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link text-white fs-4 mx-4" href="./LoginAd.php">Logout</a>
+                        <a class="nav-link text-white fs-4 mx-4" href="./includes/logout.inc.php">Logout</a>
                     </li>
 
                 </ul>
